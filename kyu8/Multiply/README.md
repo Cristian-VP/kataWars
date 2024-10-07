95d4888b29d812b520463f78948649f52b82acf0
Cristian-VP <cvacacelaprocel@cifpfibmoll.eu>
2024-10-07 09:54:45 +0200

    Solucionado el fallo

---
+++ kyu8/Multiply/README.md
@@ -0,0 +1,3 @@
+#Multiply - 8 kyu
+
+-This code does not execute properly. Try to figure out why.
---
+++ kyu8/Multiply/main.java
@@ -0,0 +1,7 @@
+//La líena 5 de la clase AgeDiff no estaba cerrada con ";"
+
+public class AgeDiff {
+    public static String CalculateAge(int birth, int yearTo) {
+        return "";
+    }
+}
